bef9a7add39f98aa7bd3e3c8f3e44750cc481f8c
Amalraj P V <amalrajpv001@gmail.com>
2022-03-20 14:43:44 +0530

    coding format with for loop

--- generator.py
+++ generator.py
@@ -12,13 +12,11 @@ height = width = 0
 pixel_arr = []
 quality = 1
 
-def rgb_to_hex(r, g, b):
-        return f'#{r:02x}{g:02x}{b:02x}'
-
 
 def add():
     global image, height, width, pixel_arr
-    path = askopenfilename(filetypes=[('Image Files', ['*.jpeg', '*.png', '*jpg'])])
+    path = askopenfilename(
+        filetypes=[('Image Files', ['*.jpeg', '*.png', '*jpg'])])
     if path:
         path_name.set(path)
         image = Image.open(path)
@@ -34,7 +32,6 @@ def add():
         qlty.grid(row=0, column=0, pady=10, ipady=2, sticky=EW)
 
 
-
 def clear():
     f = open('source.py', 'w')
     f.write('')
@@ -45,22 +42,20 @@ def generate():
     global image, height, width, pixel_arr, quality
     clear()
     quality = 11 - selection.get()
+    funstg = '\'#{r:02x}{g:02x}{b:02x}\''
+    funcallstg = '{rgb_to_hex(data[i][j][0], data[i][j][1], data[i][j][2])}'
     try:
         data = np.array(pixel_arr).reshape((height, width, 3))
     except:
         data = np.array(pixel_arr).reshape((height, width, 4))
     f = open('source.py', 'a')
-    f.write(f'from tkinter import *\nroot = Tk()\nc = Canvas(root, height={height}, width={width}, background="black")\nc.pack()\n')
-    for i in range(0, height, quality):
-        for j in range(0, width, quality):
-            if len(data[i][j]) == 4:
-                if data[i][j][3] == 0:
-                    continue
-            f.write(f'c.create_rectangle({j}, {i}, {j+quality}, {i+quality}, fill="{rgb_to_hex(data[i][j][0], data[i][j][1], data[i][j][2])}", outline="")\n')
-    f.write('root.mainloop()')
+    f.write(
+        f'from tkinter import *\nroot = Tk()\nc = Canvas(root, height={height}, width={width}, background="black")\nc.pack()\ndata={data.tolist()}\ndef rgb_to_hex(r,g,b):\n return f{funstg}\nfor i in range(0, {height}, {quality}):\n for j in range(0, {width}, {quality}):\n  c.create_rectangle(j, i, j+{quality}, i+{quality}, outline="", fill=f"{funcallstg}")\nroot.mainloop()'
+        )
     f.close()
     cbtn.grid(row=4, column=0)
-    messagebox.showinfo("Success", "Code generate. You can copy it from source.py")
+    messagebox.showinfo(
+        "Success", "Code generate. You can copy it from source.py")
 
 
 def copy_text():
@@ -84,10 +79,12 @@ root.grid_columnconfigure(0, weight=1)
 path_name = StringVar()
 
 
-head = Label(root, text="Generate Tkinter Canvas", font=('bold', 20), background="#56CD63")
+head = Label(root, text="Generate Tkinter Canvas",
+             font=('bold', 20), background="#56CD63")
 head.grid(row=0, column=0, pady=5, sticky=EW)
 
-info = Label(root, text="Add image to generate the source code and copy it from source.py", font=('italic', 10))
+info = Label(
+    root, text="Add image to generate the source code and copy it from source.py", font=('italic', 10))
 info.grid(row=1, column=0, pady=10, sticky=EW)
 
 fr1 = Frame(root, pady=10)
